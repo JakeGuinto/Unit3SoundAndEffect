7c343f585cadfd8a85952fa5c6d681012fb8494d
JakeGuinto <116121157+JakeGuinto@users.noreply.github.com>
2023-03-23 15:29:03 -0700

    JumpAndBackground
    Finished jump force section and began on background scroll.

---
+++ Unit3-SoundAndEffect/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpforce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    private bool gameover = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        { 
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isOnGround = true;
+        else if (collision.gameObject.CompareTag("Obstacle"))
+            Debug.Log("Game Over");
+    }
+}
