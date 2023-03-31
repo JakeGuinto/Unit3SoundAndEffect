e9c5cd0fabf46f9f407b7ca786b9f5f852537909
JakeGuinto <116121157+JakeGuinto@users.noreply.github.com>
2023-03-31 15:25:27 -0700

    BonusFeatures
    Finished main part of Unit-3 and Bonus Features

--- Unit3-SoundAndEffect/Assets/Scripts/PlayerController.cs
+++ Unit3-SoundAndEffect/Assets/Scripts/PlayerController.cs
@@ -8,31 +8,75 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpforce = 10;
     public float gravityModifier;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
     public bool isOnGround = true;
-    private bool gameover = false;
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
+    public bool doubleSpeed = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         { 
             playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+            doubleJumpUsed = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
+        { 
             isOnGround = true;
-        else if (collision.gameObject.CompareTag("Obstacle"))
+            dirtParticle.Play();
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+
+        { 
             Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            dirtParticle.Stop();
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
     }
 }
--- Unit3-SoundAndEffect/Assets/Scripts/SpawnManager.cs
+++ Unit3-SoundAndEffect/Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+    private int randomObstacle;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,7 +26,13 @@ public class SpawnManager : MonoBehaviour
     }
 
     void SpawnObstacle ()
-    {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    { 
+        if (playerControllerScript.gameOver == false)
+        {
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos,
+obstaclePrefabs[randomObstacle].transform.rotation);
+        }
+
     }
 }
